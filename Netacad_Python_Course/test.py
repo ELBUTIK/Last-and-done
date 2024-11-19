b8cd28ee100cbd2ccd4097ac93a5ed038de051cb
Lasha Butikashvili <35690053+ELBUTIK@users.noreply.github.com>
2024-11-19 11:34:09 +0400

    Update test.py

--- Netacad_Python_Course/test.py
+++ Netacad_Python_Course/test.py
@@ -1,1 +1,13 @@
-print("Test")
+# print("Test")
+
+# print("Another test")
+
+
+# print("The itsy bitsy spider\nclimbed up the waterspout.", end=" ")
+# print("Down came the rain\nand washed the spider out.")
+
+
+# print("The itsy bitsy spider","climbed up" , "the waterspout.")
+
+
+print("The itsy bitsy spider","climbed up" , "the waterspout.", sep='-')
