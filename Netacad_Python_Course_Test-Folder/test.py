3157dca2b33c733f3993c39abc342f4f12dc93c2
Lasha Butikashvili <35690053+ELBUTIK@users.noreply.github.com>
2024-11-19 22:15:03 +0400

    11/19/24

---
+++ Netacad_Python_Course_Test-Folder/test.py
@@ -0,0 +1,55 @@
+# print("Test")
+
+# print("Another test")
+
+
+# print("The itsy bitsy spider\nclimbed up the waterspout.", end=" ")
+# print("Down came the rain\nand washed the spider out.")
+
+
+# print("The itsy bitsy spider","climbed up" , "the waterspout.")
+
+
+print("The itsy bitsy spider","climbed up","the waterspout.", end=" ", sep=',')
+print("end")
+
+
+
+# print(0x202)
+# print(0x212)
+# print(0x222)
+# print(0x201)
+
+# print(0x5)
+# print(0x25)
+
+print(0o145)
+
+print(0o24)
+
+print(0o325)
+
+
+print(3 * 3e5)
+
+
+
+
+print(100000)
+
+print(10 * 10e3)
+print(10e4)
+
+print(0.00000000000000001)
+
+
+
+print("I like the book called - \"1984\"")
+
+
+print(2*3)
+print(2**3)
+
+
+print(5/2)
+print(5//2.)
